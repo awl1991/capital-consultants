d0b4b1bcd179a7a8b3fadacd20826dc12a0fbf3d
Andrew Labunski <awl1991@gmail.com>
2019-11-22 19:50:24 -0600

    removed png brackets

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
-import left from "../images/left_bracket.png"
-import right from "../images/right_bracket.png"
-
-const bracketStyle = {
-  opacity: ".5",
-  marginBottom: "-.32rem",
-  height: "1.2rem"
-}
 
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: "none" } : {}}>
     <p className="copyright">
-      <img style={bracketStyle} src={left} />
-      {"\t"}
-      &copy; Capital Consultants L.L.C. │ Plano, TX ─ USA
-      {"\t"}
-      <img style={bracketStyle} src={right} />
+      ╢&copy; Capital Consultants L.L.C. │Plano, TX ─ USA╟
     </p>
   </footer>
 )
