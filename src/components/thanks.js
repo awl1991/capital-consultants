f61c96dad74388a2762684ab3102dbc24daa3f06
Andrew Labunski <awl1991@gmail.com>
2019-11-26 12:06:42 -0600

    netlify SSG form setup

--- src/components/thanks.js
+++ src/components/thanks.js
@@ -2,8 +2,8 @@ import React from "react"
 import Layout from "../layout"
 
 export default () => (
-  <Layout>
+  <div>
     <h1>Thank you!</h1>
     <p>This is a custom thank you page for form submissions</p>
-  </Layout>
+  </div>
 )
