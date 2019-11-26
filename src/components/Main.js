b86a6176188885da513e9b85602a4f21e77d6ff5
Andrew Labunski <awl1991@gmail.com>
2019-11-26 03:31:31 -0600

    edited post

--- src/components/Main.js
+++ src/components/Main.js
@@ -112,7 +112,7 @@ class Main extends React.Component {
           <p>
             Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
             libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
+            Pellentesque condiment sem. In efficitur ligula tate urna.
             Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
             Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
             libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
