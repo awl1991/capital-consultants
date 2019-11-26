9da3ea3c71eb8d80a2a08e013edaf117f4bcbebc
Andrew Labunski <awl1991@gmail.com>
2019-11-26 00:40:29 -0600

    form setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -113,18 +113,14 @@ class Main extends React.Component {
           style={{ display: "none" }}
         >
           <h3 className="major">Contact</h3>
-          <form
-            subject={"New Form Submission"}
-            method="POST"
-            data-netlify="true"
-          >
+          <form method="post" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="email" name="email" id="email" />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
