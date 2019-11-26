b923edbe05c2f6baa31e8c6394ed8b2f35f8b207
Andrew Labunski <awl1991@gmail.com>
2019-11-25 22:30:35 -0600

    Netlify form data

---
+++ src/assets/post/index.js
@@ -0,0 +1,7 @@
+$("#contact").submit(function(e) {
+  e.preventDefault()
+  var $form = $(this)
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Thank you!")
+  })
+})
