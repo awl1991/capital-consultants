6b65a1d8716378d10fb7a6c7303586db5611df16
Andrew Labunski <awl1991@gmail.com>
2019-11-27 01:16:45 -0600

    commit all

--- src/components/thanks.js
+++ src/components/thanks.js
@@ -1,8 +1,3 @@
-import React from "react"
-
-export default () => (
-  <div>
-    <h1>Thank you! Page</h1>
-    <p>This is a sample page.</p>
-  </div>
-)
+;() => {
+  alert("Thanks for contacting us!")
+}
