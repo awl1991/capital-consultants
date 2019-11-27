3dfebf5c806d5b16cab67baed7d2907d30685e79
Andrew Labunski <awl1991@gmail.com>
2019-11-27 01:41:45 -0600

    commit all

--- src/components/Main.js
+++ src/components/Main.js
@@ -114,12 +114,7 @@ const Main = props => {
           data-netlify="true"
           data-netlify-honeypot="bot-field"
         >
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" />
-            </label>
-          </p>
+          <input hidden className="hidden" name="bot-field" />
           <div className="field half first">
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" />
