d6cdf200486ef1beaf7463057eae9fb3df3b11f5
Andrew Labunski <awl1991@gmail.com>
2019-11-27 01:33:46 -0600

    commit all

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,34 +4,7 @@ import { navigate } from "gatsby-link"
 import pic02 from "../images/pic02.png"
 import pic03 from "../images/pic03.jpg"
 //import pic01 from "../images/pic01.jpg"
-
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&")
-}
-
 const Main = props => {
-  const [state, setState] = React.useState({})
-
-  const handleChange = e => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        contact: form.getAttribute("name"),
-        ...state
-      })
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch(error => alert(error))
-  }
   let close = (
     <div
       className="close"
@@ -147,31 +120,20 @@ const Main = props => {
           <input type="hidden" name="form-name" value="contact" />
           <p hidden>
             <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={handleChange} />
+              Don’t fill this out: <input name="bot-field" />
             </label>
           </p>
           <div className="field half first">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" onChange={handleChange} />
+            <input type="text" name="name" id="name" />
           </div>
           <div className="field half">
             <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              onChange={handleChange}
-            />
+            <input type="text" name="email" id="email" />
           </div>
           <div className="field">
             <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              rows="4"
-              onChange={handleChange}
-            ></textarea>
+            <textarea name="message" id="message" rows="4"></textarea>
           </div>
           <ul className="actions">
             <li>
