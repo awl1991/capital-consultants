03b2f2e0e27ed6c719ef20693b1d86348aa2c56c
Andrew Labunski <awl1991@gmail.com>
2019-11-29 17:41:02 -0600

    removed theme plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,7 @@ module.exports = {
     title: "Capital Consultants",
     author: "Andrew Labunski",
     description: "Large Loss Consulting Agency",
-    icon: ""
+    icon: "src/images/logo.png"
   },
-  plugins: [
-    {
-      options: {
-        start_url: "/",
-        background_color: "#000",
-        theme_color: "#000",
-        icon: "src/images/logo.png" // This path is relative to the root of the site.
-      }
-    },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-offline"
-  ]
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-offline"]
 }
