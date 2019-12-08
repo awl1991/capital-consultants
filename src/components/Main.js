e12651b22a8035b2fd62f2876869b584fb74f268
Andrew Labunski <awl1991@gmail.com>
2019-12-08 01:05:04 -0600

    ...

--- src/components/Main.js
+++ src/components/Main.js
@@ -90,11 +90,11 @@ const Main = props => {
         </span>
         <p>
           Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-          eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat
-          volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam.
-          Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero
-          et malesuada fames ac ante ipsum primis in faucibus. Cras viverra
-          ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.
+          eleifend, at eleifend sapien imperdiet ac. Aliquam erat volutpat.
+          Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer
+          sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et
+          malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula
+          sit amet ex mollis mattis lorem ipsum dolor sit amet.
         </p>
         {close}
       </article>
