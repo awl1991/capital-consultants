52c2127ac796e68a54977be525a5d48fe44d003c
Andrew Labunski <awl1991@gmail.com>
2020-01-13 18:09:03 -0600

    logo hover animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import shadowLogo from "../images/logo_w_shadow.png"
 
-const logoStyle = {
-	position: "absolute",
-	left: 0,
-	right: 0,
-	marginLeft: "auto",
-	marginRight: "auto",
-	height: "100%",
-	opacity: "1"
-}
-
 const Header = props => (
 	<header id="header" style={props.timeout ? { display: "none" } : {}}>
 		<div className="logo">
-			<img style={logoStyle} src={shadowLogo} alt="Capital Consultants" />
+			<img className="logoStyle" src={shadowLogo} alt="Capital Consultants" />
 		</div>
 		<div className="content">
 			<div className="inner">
